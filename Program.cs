06e7b70eadecfd226f0136db94227a4e91f2b460
JLewis947 <29450687+JLewis947@users.noreply.github.com>
2020-10-08 14:44:28 +0100

    Changed how function detects and removes punctuation to a more easier method. Added in function sort text into alphabetical order.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace LabExercise01
 {
@@ -7,10 +10,10 @@ namespace LabExercise01
     {
         static void Main(string[] args)
         {
+            string data = null;
+            var sb = new StringBuilder();
             // Check if file exists
             if (File.Exists("input.txt")) Console.WriteLine("File Exists");
-            string data = null;
-            char empty = ' ';
             // Read file text to string
             using (var reader = new StreamReader("input.txt"))
             {
@@ -19,13 +22,17 @@ namespace LabExercise01
             // Remove punctuation from string
             foreach (char c in data)
             {
-                if (c == '!' || c == '.' || c == ',' || c == '?')
+                if (!char.IsPunctuation(c))
                 {
-                    data = data.Replace(c, empty);
+                    sb.Append(c);
                 }
             }
+            data = sb.ToString();
             // Make all letters lowercase
             data = data.ToLower();
+            // Sort words into alphabetical order
+            List<string> words = data.Split(" ").ToList();
+            words.Sort();
             // Write string to console
             Console.WriteLine(data);
         }
