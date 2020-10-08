688a99e40e0772cafa8ba49c34d64b17301f8657
JLewis947 <29450687+JLewis947@users.noreply.github.com>
2020-10-08 13:06:51 +0100

    Added comments to understand what is happening in program. Removed punctuation from string.

--- Program.cs
+++ Program.cs
@@ -7,12 +7,25 @@ namespace LabExercise01
     {
         static void Main(string[] args)
         {
+            // Check if file exists
             if (File.Exists("input.txt")) Console.WriteLine("File Exists");
             string data = null;
+            char empty = ' ';
+            // Read file text to string
             using (var reader = new StreamReader("input.txt"))
             {
                 data = reader.ReadToEnd();
             }
+            // Remove punctuation from string
+            foreach (char c in data)
+            {
+                if (c == '!' || c == '.' || c == ',' || c == '?')
+                {
+                    data = data.Replace(c, empty);
+                }
+            }
+            // Write string to console
+            Console.WriteLine(data);
         }
     }
 }
